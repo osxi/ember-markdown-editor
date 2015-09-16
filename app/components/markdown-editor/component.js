2ad51adb3ddf0514f59cb30de59d2e41726b5f64
osxi <osxi@users.noreply.github.com>
2015-09-16 00:28:21 -0500

    add field to retrieve markdown from a github repo

--- app/components/markdown-editor/component.js
+++ app/components/markdown-editor/component.js
@@ -1,8 +1,30 @@
 import Ember from 'ember';
+import icAjax from 'ic-ajax';
 import marked from 'npm:marked';
 
 export default Ember.Component.extend({
   classNames: ['markdown-editor'],
+
+  repo: 'osxi/ember-markdown-editor',
+  validRepo: true,
+
+  didInsertElement() {
+    if (!Ember.isBlank(this.get('repo'))) {
+      this.send('retrieveMarkdown');
+    }
+  },
+
+  actions: {
+    retrieveMarkdown() {
+      var repo = this.get('repo');
+      var url = `https://rawgit.com/${repo}/master/README.md`;
+
+      return icAjax(url).then(md => {
+        this.set('validRepo', true)
+        this.set('markdown', md);
+      }).catch(() => this.set('validRepo', false));
+    }
+  },
 
   _keyUpObserver: function() {
     var html = marked(this.get('markdown') || '');
